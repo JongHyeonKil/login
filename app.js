7c9782bb3f05a23c53448b6072e4ec9afccadd07
JongHyeonKil <rlfwhdgus7717@naver.com>
2022-06-11 20:00:06 +0900

    로그인 기능 구현

---
+++ app.js
@@ -0,0 +1,17 @@
+// 모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// 라우팅
+const home = require("./routes/home");
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// 미들웨어 등록 use
+app.use("/", home);
+
+module.exports = app;
